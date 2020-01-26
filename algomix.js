b9ee723d6489c0a1cf28d875794798a699af5d3f
marcusjohnsson <mr.marcus.marega@gmail.com>
2020-01-26 09:16:42 +0100

    first two

---
+++ algomix.js
@@ -0,0 +1,19 @@
+// Convert Celcius Temperature to Farenheit
+// @param int, return int
+function convertCelciusToFarenheitF(celsius) {
+  let farenheit = (celsius * 9) / 5 + 32;
+  return farenheit;
+}
+
+// Reverses order of string
+// @param str, return str
+function reverseString(str) {
+  let l = str.length - 1;
+  let reversedString = "";
+  reversedString;
+  while (l > -1) {
+    reversedString += str[l];
+    l--;
+  }
+  return reversedString;
+}
